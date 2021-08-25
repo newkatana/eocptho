b119f9ece6b5d07c522eef42fe820f82b81485b5
newkatana <kikyukatana@gmail.com>
2021-08-25 11:48:22 +0700

    test

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-  	<title>EOC</title>
+  	<title>EOC::</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
